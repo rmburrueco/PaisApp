0f76ed508d45e918cd4f8d968b0df1cd25f53e8d
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-04-18 14:05:05 +0200

    Añadir contenido a la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,8 +10,9 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent{
 
-  termino: string = '';
+  termino : string = '';
   hayError: boolean = false;
+  paises  : Country[] = [];
 
   constructor(private paisService: PaisService ) { }
   
@@ -21,8 +23,10 @@ export class PorPaisComponent{
     this.paisService.buscarPais( this.termino )
     .subscribe( paises => {
         console.log(paises);
+        this.paises = paises;        
     }, (err) => {
       this.hayError = true;
+      this.paises   = [];
     });
   }
 
