cfaf19e4b37703e96ad9636ebf552541d9d263e2
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-05-06 10:16:40 +0200

    Sugerencias

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent{
@@ -13,6 +18,8 @@ export class PorPaisComponent{
   termino : string = '';
   hayError: boolean = false;
   paises  : Country[] = [];
+  paisesSugeridos  : Country[] = [];
+  mostrarSugerencias : boolean = false;
 
   constructor(private paisService: PaisService ) { }
   
@@ -32,6 +39,19 @@ export class PorPaisComponent{
 
   sugerencias( termino:string ){
     this.hayError = false;
-    // TODO: crear sugerencias
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+    .subscribe( paises => this.paisesSugeridos = paises.splice(0,5),
+    (err) => this.paisesSugeridos = []
+    );
+
+  }
+
+  buscarSugerido(termino:string){
+    this.mostrarSugerencias = false;
+    this.buscar(termino);
+
   }
 }
